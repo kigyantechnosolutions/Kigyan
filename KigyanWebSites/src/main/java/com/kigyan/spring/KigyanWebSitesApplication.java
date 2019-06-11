73adb2da5ba0df4377f920fe93f457151ce16d79
Santosh Begur <santoshbegur@gmail.com>
2019-06-11 15:35:04 +0530

    Initial Commit

---
+++ KigyanWebSites/src/main/java/com/kigyan/spring/KigyanWebSitesApplication.java
@@ -0,0 +1,13 @@
+package com.kigyan.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KigyanWebSitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KigyanWebSitesApplication.class, args);
+	}
+
+}
